88a8f8d89d74e25e65e9574ac70e77a0e88b0940
Luke Southam <luke@devthe.com>
2013-11-10 14:41:04 +0000

    Added week 1 solutions 1 and 2.

---
+++ solutions/week1/1/SmallestPrimeFactor.pas
@@ -0,0 +1,46 @@
+program SmallestPrimeFactor;
+
+function IsPrime(Prime: Integer): Boolean;
+// Determine if a number is prime by brute force.
+// Test if any number from 2 up to a half of it is a factor.
+// If a factor exists it is prime. 
+var
+  Factor: Integer;
+begin
+  IsPrime := True;
+  for Factor := 2 to Integer(Prime DIV 2) do
+    if (Prime MOD Factor) = 0 then
+      IsPrime := False; 
+end;
+
+var
+  Number: Integer;
+  PrimeFactor: Integer = 2; // 2 is smallest prime
+
+begin
+  Write('Please enter a positive integer: ');
+  Read(Number);
+
+  if (Number = 1) or (Number = -1) then
+  begin
+    // All prime numbers are, by definition, greater than one.
+    // So 1 and -1 do not have any prime factors.
+    Writeln('Smallest Prime Factor: N/A');
+    // Exit here to not run into factor finding code.
+    // As no prime factors of 1 or -1 exist.
+    Readln();
+    Exit();
+  end;
+
+  while (Number MOD PrimeFactor) <> 0 do
+  // While prime is not factor of number.
+  begin
+    // Get next prime.
+    PrimeFactor := PrimeFactor + 1;
+    while not IsPrime(PrimeFactor) do
+      PrimeFactor := PrimeFactor + 1;
+  end;
+  Writeln('Smallest Prime Factor: ', PrimeFactor);
+
+  Readln
+end.
---
+++ solutions/week1/2/LargestPrimeFactor.pas
@@ -0,0 +1,49 @@
+program LargestPrimeFactor;
+
+function IsPrime(Prime: Integer): Boolean;
+// Determine if a number is prime by brute force.
+// Test if any number from 2 up to a half of it is a factor.
+// If a factor exists it is prime. 
+var
+  Factor: Integer;
+begin
+  IsPrime := True;
+  for Factor := 2 to Integer(Prime DIV 2) do
+    if (Prime MOD Factor) = 0 then
+      IsPrime := False; 
+end;
+
+var
+  Number, Largest: Integer;
+  PrimeFactor: Integer = 1; // prime numbers must be greater than 1
+
+begin
+  Write('Please enter a positive integer: ');
+  Read(Number);
+
+  if (Number = 1) or (Number = -1) then
+  begin
+    // All prime numbers are, by definition, greater than one.
+    // So 1 and -1 do not have any prime factors.
+    Writeln('Largest Prime Factor: N/A');
+    // Exit here to not run into factor finding code.
+    // As no prime factors of 1 or -1 exist.
+    Readln();
+    Exit();
+  end;
+
+  while PrimeFactor < Number do
+  begin
+    // Get next prime.
+    PrimeFactor := PrimeFactor + 1;
+    while not IsPrime(PrimeFactor) do
+      PrimeFactor := PrimeFactor + 1;
+
+    if (Number MOD PrimeFactor) = 0 then
+      Largest := PrimeFactor;
+  end;
+
+  Writeln('Largest Prime Factor: ', Largest);
+
+  Readln
+end.
